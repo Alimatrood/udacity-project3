ce843fe7f2577448d5ded0aaf4b0e48d11db9ce6
ali <alisaeedalmatrood@gmail.com>
2023-01-19 09:36:24 +0300

    modified config

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     DEBUG = True
     POSTGRES_URL="techconfdb1.postgres.database.azure.com"  #Updated Value
-    POSTGRES_USER="azureadmin@techconfdb1" #Updated Value
+    POSTGRES_USER="azureuser@techconfdb1" #Updated Value
     POSTGRES_PW="Azure123"   #Updated Value
     POSTGRES_DB="techconfdb"   #Updated Value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=w/sh5MSIiBRdR305bUnD8dnpbTyDW/OCsI+tulUgAFI=' #Updated Value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Bzk03D8Rv1XoXFx29zHG+4ZuBwyir87CIj7EM6/6+mA=' #Updated Value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
